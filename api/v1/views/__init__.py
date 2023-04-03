0c545ecce975173df3a61d73a9d7b467f5e6ed58
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-04-02 18:44:16 -0700

    __init__.py update made

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,11 @@
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from models import storage
+from models.state import State
+from models.city import City
 
+"""import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
