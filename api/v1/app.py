b2c7fca9ab647c7566a914ed32aa80353ec8e2bf
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-04-02 18:09:07 -0700

    In api/v1/app.py, create a handler for 404 errors that returns a JSON-formatted 404 status code response. The content should be: "error": "Not found"

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
-from flask import Flask
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Closes storage session """
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
 
-if __name__ == '__main__':
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
-    app.run(host=host, port=port, threaded=True)
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
