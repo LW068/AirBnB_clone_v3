25339d1ef84af4fd2bbbb4ffdb2929eef04a5059
LW068 <105394574+LW068@users.noreply.github.com>
2023-04-04 16:50:38 -0500

    [Fixed]: states.py to pass errors. #5 indention

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" View for State objects that handles default API actions """
+"""
+View for State objects that handles default API actions
+"""
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
@@ -8,37 +10,44 @@ from models.state import State
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
-    """ Retrieves the list of all State objects """
+    """
+    Retrieves the list of all State objects
+    """
     d_states = storage.all(State)
     return jsonify([obj.to_dict() for obj in d_states.values()])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def r_state_id(state_id):
-    """ Retrieves a State object """
-    state = storage.get("State", state_id)
-    if not state:
+    """
+    Retrieves a State object
+    """
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def del_state(state_id):
-    """ Deletes a State object """
-    state = storage.get("State", state_id)
-    if not state:
+    """
+    Deletes a State object
+    """
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
-    state.delete()
+    storage.delete(state)
     storage.save()
     return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
-    """ Creates a State object """
+    """
+    Creates a State object
+    """
     new_state = request.get_json()
-    if not new_state:
+    if new_state is None:
         abort(400, "Not a JSON")
     if "name" not in new_state:
         abort(400, "Missing name")
@@ -50,13 +59,15 @@ def post_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
-    """ Updates a State object """
-    state = storage.get("State", state_id)
-    if not state:
+    """
+    Updates a State object
+    """
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
 
     body_request = request.get_json()
-    if not body_request:
+    if body_request is None:
         abort(400, "Not a JSON")
 
     for k, v in body_request.items():
