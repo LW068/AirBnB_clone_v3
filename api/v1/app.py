18253008417451b8a9bc07cf04dd3c9d000d4a15
LW068 <105394574+LW068@users.noreply.github.com>
2023-04-04 22:03:55 -0500

    #3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
-"""API module"""
+""" Script that creates a Flask app """
 from flask import Flask, jsonify
-from os import getenv
-from api.v1.views import app_views
 from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flasgger import Swagger
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+swagger = Swagger(app)
 
 @app.teardown_appcontext
-def close_storage_db(error):
-    """Closes the storage"""
+def teardown_session(exception):
+    """ Closes storage session """
     storage.close()
 
-@app.route('/api/v1/status')
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns JSON response with 404 status """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.route('/api/v1/status', strict_slashes=False)
 def status():
-    """Returns the status of the API"""
-    return jsonify(status="OK")
+    """Returns a JSON with the status"""
+    return jsonify({"status": "OK"})
+
 
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+
+    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
+    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
     app.run(host=host, port=port, threaded=True)
