bb0671a9d833d2902fddbce6b754dee17fdbf7cb
LW068 <105394574+LW068@users.noreply.github.com>
2023-04-04 22:00:35 -0500

    apy.py gtp version

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
 """ Script that imports a Blueprint and runs Flask """
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flasgger import Swagger
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['SWAGGER'] = {
-    "swagger_version": "2.0",
-    "title": "Flasgger",
-    "headers": [
-        ('Access-Control-Allow-Origin', '*'),
-        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
-        ('Access-Control-Allow-Credentials', "true"),
-    ],
-    "specs": [
-        {
-            "version": "1.0",
-            "title": "HBNB API",
-            "endpoint": 'v1_views',
-            "description": 'HBNB REST API',
-            "route": '/v1/views',
-        }
-    ]
-}
-swagger = Swagger(app)
+
 
 @app.teardown_appcontext
 def teardown_session(exception):
@@ -39,7 +20,13 @@ def teardown_session(exception):
 @app.errorhandler(404)
 def not_found(error):
     """ Returns JSON response with 404 status """
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.route('/api/v1/status')
+def status():
+    """ Returns JSON response with status """
+    return jsonify({"status": "OK"})
 
 
 if __name__ == '__main__':
