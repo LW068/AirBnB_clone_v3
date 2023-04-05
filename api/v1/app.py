6c667f65341a9513bb2666cccd411f598e2d4470
LW068 <105394574+LW068@users.noreply.github.com>
2023-04-04 22:02:11 -0500

    second try

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
+"""API module"""
 from flask import Flask, jsonify
-from flask_cors import CORS
-from models import storage
-from api.v1.views import app_views
 from os import getenv
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Closes storage session """
+def close_storage_db(error):
+    """Closes the storage"""
     storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """ Returns JSON response with 404 status """
-    return jsonify({"error": "Not found"}), 404
-
 
 @app.route('/api/v1/status')
 def status():
-    """ Returns JSON response with status """
-    return jsonify({"status": "OK"})
-
+    """Returns the status of the API"""
+    return jsonify(status="OK")
 
 if __name__ == '__main__':
-    HBNB_API_HOST = getenv('HBNB_API_HOST')
-    HBNB_API_PORT = getenv('HBNB_API_PORT')
-
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
     app.run(host=host, port=port, threaded=True)
